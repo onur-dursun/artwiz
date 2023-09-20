f3e7f48aa8be1d15878c15f164fa7a76930216b9
onur-dursun <dursunon@itu.edu.tr>
2023-09-20 04:13:52 +0300

    0.0.0.2

--- commands/image/Invert Colors.bat
+++ commands/image/Invert Colors.bat
@@ -1,2 +1,2 @@
 magick "%~dpnx1" -channel RGB -negate "%~dpn1_inverted.png"
-explorer.exe /select, "%~dpn1_inverted.png"
+explorer.exe /select, %~dpn1_inverted.png
--- commands/video/compressvideo.bat
+++
@@ -1,1 +0,0 @@
-ffmpeg -i %~dpnx1 -vcodec h264 -acodec aac %~dpn1_compressed.mp4
